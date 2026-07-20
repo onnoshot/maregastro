e3e0682522f48b9a5f8f32ee31add8df4197c12f
onnoshot <onurxvala@gmail.com>
2026-07-20 16:01:50 +0300

    fix(saatler): calisma gunleri Pazartesi haric tum gun acik olarak guncellendi
    Sadece Pazartesi kapali; Sali-Pazar 24 saat acik (once Cuma-Pazar acikti).
    Site metni + Restaurant schema openingHoursSpecification 4 dilde guncellendi.

--- i18n/build_i18n.py
+++ i18n/build_i18n.py
@@ -102,7 +102,7 @@ def build_restaurant_schema(lang):
         "geo": {"@type": "GeoCoordinates", "latitude": 40.7002438, "longitude": 30.222254},
         "openingHoursSpecification": [{
             "@type": "OpeningHoursSpecification",
-            "dayOfWeek": ["Friday", "Saturday", "Sunday"],
+            "dayOfWeek": ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
             "opens": "00:00",
             "closes": "23:59",
         }],
